5227b0b336901b81da7bd26383f888104e48e664
Gauatm-raj <gautamraj7569@gmail.com>
2024-06-18 12:02:56 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const [counter,setState]= useState(0);
+
+   const clicked=()=>{
+     setState((prev)=>prev+1)
+   }
   return (
     <div>
-        {/* Do not remove the main div */}
+      <p style={{ fontWeight: "bold"}}>Button clicked {counter} times</p>
+      <button onClick={clicked}>Click me</button>
+      
     </div>
   )
 }
